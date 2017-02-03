76410c8146e378ab925a02b7a4b60ca225f85c6c
deadlocker <deadlocker@gmx.de>
2017-02-03 15:42:27 +0100

    added method to convert InputStream to String

--- src/tools/Read.java
+++ src/tools/Read.java
@@ -155,4 +155,45 @@ public class Read
 		
 		return lines;	
 	}
+	
+	/**
+	 * converts an InputStream to a String
+	 * @param is - InputStream
+	 * @return String
+	 */
+	public static String getStringFromInputStream(InputStream is)
+	{
+		BufferedReader br = null;
+		StringBuilder sb = new StringBuilder();
+
+		String line;
+		try
+		{
+			br = new BufferedReader(new InputStreamReader(is));
+			while((line = br.readLine()) != null)
+			{
+				sb.append(line);
+			}
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			if(br != null)
+			{
+				try
+				{
+					br.close();
+				}
+				catch(IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return sb.toString();
+	}
 }
